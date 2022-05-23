44885d1dc1b1f17ec81038da6f17ba6c9327ae68
Michelle Tambi <michelletambi22@gmail.com>
2022-05-23 14:00:16 +0100

    Becca's changes

--- PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/PokemonBackEndProjectApplicationTests.java
+++ PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/PokemonBackEndProjectApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.PokemonBackEndProject;
 
+import com.example.PokemonBackEndProject.model.Pokemon;
+import com.example.PokemonBackEndProject.repository.PokemonRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
 
 @SpringBootTest
 class PokemonBackEndProjectApplicationTests {
 
+	@Autowired
+	PokemonRepository pokemonRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void	canFindPokemonButterfree() {
+		List<Pokemon> found = pokemonRepository.findByName("Butterfree");
+		assertThat(found).isEqualTo("Butterfree");
 	}
 
 }
