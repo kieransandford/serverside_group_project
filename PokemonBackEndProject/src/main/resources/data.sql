41b4068599ea8b1e2875b8098c4c67b06e77d531
Michelle Tambi <michelletambi22@gmail.com>
2022-05-24 15:47:58 +0100

    Added enums to data.sql

--- PokemonBackEndProject/src/main/resources/data.sql
+++ PokemonBackEndProject/src/main/resources/data.sql
@@ -3,22 +3,19 @@ INSERT INTO Trainer (id, name, age, town) VALUES (2,'Kieran',24,'Manchester');
 INSERT INTO Trainer (id, name, age, town) VALUES (3,'Naeem',23,'Liverpool');
 INSERT INTO Trainer (id, name, age, town) VALUES (4,'Becca',26,'Hertfordshire');
 
-INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(1, 'Butterfree', 'Bug-Flying', 1, 1);
-INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(2, 'Raticate', 'Normal',1, 2);
-INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(3, 'Oddish', 'Grass-Poison',1, 4) ;
-INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(4, 'Drowzee', 'Psychic',1, 4);
-INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(5, 'Lickitung', 'Normal',1, 3);
-
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(6, 'Chikorita', 'Grass', 2, 1);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(7, 'Croconaw', 'Water', 2, 2);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(8, 'Furret', 'Normal', 2, 3);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(9, 'Crobat', 'Poison, Flying', 2, 4);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(10, 'Flaaffy', 'Elecrtic', 2, 2);
-
-
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(16, 'Piplup', 'Water', 4, 2);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(17, 'Chatot', 'Normal, Flying', 4, 3);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(18, 'Skorupi', 'Poison, Bug', 4, 3);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(19, 'Togekiss', 'Fairy, Flying', 4, 4);
-INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(20, 'Mamoswine', 'Ice, Ground', 4, 1);
+INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(1, 'Butterfree', 'Bug-Flying','KANTO' , 1);
+INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(2, 'Raticate', 'Normal','KANTO', 2);
+INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(3, 'Oddish', 'Grass-Poison','KANTO', 4) ;
+INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(4, 'Drowzee', 'Psychic','KANTO', 4);
+INSERT INTO Pokemon (ID, name, type, generation, trainer_id) VALUES(5, 'Lickitung', 'Normal','JOHTO', 3);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(6, 'Chikorita', 'Grass', 'JOHTO', 1);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(7, 'Croconaw', 'Water', 'JOHTO', 2);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(8, 'Furret', 'Normal', 'HOENN', 3);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(9, 'Crobat', 'Poison, Flying', 'HOENN', 4);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(10, 'Flaaffy', 'Elecrtic', 'HOENN', 2);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(16, 'Piplup', 'Water', 'SINNOH', 2);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(17, 'Chatot', 'Normal, Flying', 'SINNOH', 3);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(18, 'Skorupi', 'Poison, Bug', 'SINNOH', 3);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(19, 'Togekiss', 'Fairy, Flying', 'ALOLA', 4);
+INSERT INTO Pokemon (id, name, type, generation, trainer_id) VALUES(20, 'Mamoswine', 'Ice, Ground', 'ALOLA', 1);
 
