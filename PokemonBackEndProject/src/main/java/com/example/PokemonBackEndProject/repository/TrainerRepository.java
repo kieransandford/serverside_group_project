b120851064bc35a3695431123b2c25bdb53d399e
Michelle Tambi <michelletambi22@gmail.com>
2022-05-24 10:00:16 +0100

    Added findAllOfType query to TrainerRepository

--- PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/repository/TrainerRepository.java
+++ PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/repository/TrainerRepository.java
@@ -1,7 +1,14 @@
 package com.example.PokemonBackEndProject.repository;
 
+import com.example.PokemonBackEndProject.model.Pokemon;
 import com.example.PokemonBackEndProject.model.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+//    Get all Pokemon of a specific type
+    @Query(value = "SELECT * FROM POKEMON WHERE TYPE IS 'Normal' ", nativeQuery = true)
+    List<Pokemon> findAllOfType();
 }
