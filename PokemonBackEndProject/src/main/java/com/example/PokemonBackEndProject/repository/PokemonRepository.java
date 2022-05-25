b4bb76cebb4cd8bd6b664907eef103bdf4fe3601
Michelle Tambi <michelletambi22@gmail.com>
2022-05-25 08:16:23 +0100

    Added the random Pokemon query

--- PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/repository/PokemonRepository.java
+++ PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/repository/PokemonRepository.java
@@ -2,6 +2,7 @@ package com.example.PokemonBackEndProject.repository;
 
 import com.example.PokemonBackEndProject.model.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +13,8 @@ public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
     List<Pokemon> findByType(String type);
 
+    //  The random Pokemon method uses this method to find a Pokemon at a random id index
+    @Query(value = "SELECT * FROM Pokemon WHERE id = ?", nativeQuery = true)
     Optional<Pokemon> findById(Long id);
+
 }
--- PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/controller/PokemonControllerTest.java
+++ PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/controller/PokemonControllerTest.java
@@ -1,18 +1,31 @@
 package com.example.PokemonBackEndProject.controller;
 
+import com.example.PokemonBackEndProject.repository.PokemonRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PokemonController.class)
 class PokemonControllerTest {
 
-    @Autowired
-    @Test
-    void getAllPokemon() {
-    }
+//    @Autowired
+//    private MockMvc mvc;
+//    @MockBean
+//    private PokemonRepository pokemonRepository;
+//
+//    @Test
+//    public void getAllPokemon() throws Exception {
+//        RequestBuilder request = MockMvcRequestBuilders.get("/pokemon");
+//        MvcResult result = mvc.perform(request).andReturn();
+//        assertEquals(result, pokemonRepository.findAll());
+//    }
 }
