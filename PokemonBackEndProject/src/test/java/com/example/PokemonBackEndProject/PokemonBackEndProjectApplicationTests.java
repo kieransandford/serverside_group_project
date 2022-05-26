7efd7c9cba078f56619474a12c7d5ddbca3ac048
Michelle Tambi <michelletambi22@gmail.com>
2022-05-26 10:19:38 +0100

    changed findByName() to return an object, not list

--- PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/PokemonBackEndProjectApplicationTests.java
+++ PokemonBackEndProject/src/test/java/com/example/PokemonBackEndProject/PokemonBackEndProjectApplicationTests.java
@@ -12,13 +12,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class PokemonBackEndProjectApplicationTests {
-
-	@Autowired
-	private PokemonRepository pokemonRepository;
-
 	@Autowired
 	private TrainerRepository trainerRepository;
 
@@ -26,51 +23,9 @@ class PokemonBackEndProjectApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	public void	canFindPokemonButterfree() {
-		List<Pokemon> found = pokemonRepository.findByName("Butterfree");
-		assertThat(found).isEqualTo("Butterfree");
-	}
 
-	@Test
-	public void canChangeNameAndType() {
-		//Given - name
-		List<Pokemon> name = pokemonRepository.findByName("Skorupi");
-		//When - put method changes name
-		if (name.equals("Drapion")) {
 
-		//Then - return that name has changed
-			assertThat(name).isEqualTo("Drapion");
 
-		//Given - type
-			List<Pokemon> type = pokemonRepository.findByType("Poison, Bug");
-		//When - put method changes typing
-			if (type.equals("Poison, Dark")) {
-		//Then - return that it has changed its type
-				assertThat(type).isEqualTo("Poison, Dark");
-			}
 
-		}
-	}
-
-	// QUERY - Find trainers by town
-	@Test
-	public void canFindTrainerInLondon() {
-		List<Trainer> found = trainerRepository.findByTown("London");
-		assertThat(found.size()).isEqualTo(1);
-	}
-
-//	Test findByNameLike method in PokemonRepository
-	@Test
-	public void canFindByNameLike(){
-//		given pokemonRepository
-//		when:
-//		we call the method, it returns a list of Pokemon with similar names to input.
-		List<Pokemon> result = pokemonRepository.findByNameLike("ot");
-//		then:
-//		there should be 2 elements in the list as two Pokemon have names that contain "ot".
-		assertEquals(result.size(), 2);
-
-	}
 
 }
