13e9c743c21bc3a6ecb8b17139c7cc364f2c17b4
Michelle Tambi <michelletambi22@gmail.com>
2022-05-26 15:41:45 +0100

    Added the rating parameter to updatePokemon in PokemonService.

--- PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/Service/PokemonService.java
+++ PokemonBackEndProject/src/main/java/com/example/PokemonBackEndProject/Service/PokemonService.java
@@ -20,13 +20,16 @@ public class PokemonService {
     If the name/type of that pokemon is not null and is not equal to the name/type value you are trying to input, set
     them to the new values that you have input.
      */
-    public void updatePokemon(Long pokemonId, String name, String type){
+    public void updatePokemon(Long pokemonId, String name, String type, int rating){
         Pokemon pokemon = pokemonRepository.findById(pokemonId).orElseThrow(()-> new IllegalStateException("Pokemon with " +pokemonId + " Does not exist!"));
         if (pokemon.getName() != null  && !Objects.equals(pokemon.getName(), name)) {
             pokemon.setName(name);
         }
         if (pokemon.getType() != null && !Objects.equals(pokemon.getType(), type)) {
             pokemon.setType(type);
+        }
+        if (pokemon.getRating() != 0 && !Objects.equals(pokemon.getRating(), rating)) {
+            pokemon.setRating(rating);
         }
     }
 
